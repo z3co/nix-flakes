26198e022d9127ecd25c3b894a3a0d974d9895dd
Jeppe Wolff Johansen <jepper123411@proton.me>
2026-03-01 19:54:08 +0100

    Updated flake

--- templates/go/flake.nix
+++ templates/go/flake.nix
@@ -1,28 +1,29 @@
 {
-  description = "A very basic flake";
+  description = "A basic flake with go and zsh";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+		flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, ... }: 
-    flake-parts.lib.mkFlake { inherit inputs; } 
-    {
-      systems = [ "x86_64-linux" "x86_64-darwin" ];
-      imports = [
-        ./packages.nix
-      ];
-      perSystem = { pkgs, ... }: {
-          packages = with pkgs; [
-            zsh go golangci-lint gopls
-          ];
-          name = "golang";
-          shellHook = ''
-            export SHELL=${pkgs.zsh}/bin/zsh
-            exec ${pkgs.zsh}/bin/zsh
-          '';
-        };
-      };
-    };
+	outputs = inputs@{flake-parts, ...}:
+		flake-parts.lib.mkFlake {inherit inputs;} {
+			systems = [
+				"x86_64-linux"
+				"x86_64-darwin"
+			];
+			perSystem = {pkgs, ...}: {
+				devShells.default = pkgs.mkShell {
+					name = "go";
+					packages = with pkgs; [
+						go zsh
+					];
+					shellHook = ''
+						export SHELL=${pkgs.zsh}/bin/zsh
+						exec $SHELL
+						'';
+				};
+			};
+
+		};
 }
